ec16ffd45fae8cc43fa8cf2ede4edc9892372f6b
ankursrivastava11 <62030123+ankursrivastava11@users.noreply.github.com>
2020-03-10 23:05:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AutoComplete from "./components/autocomplete";
+
+ReactDOM.render(<AutoComplete />, document.getElementById("app"));
---
+++ readme.md
@@ -0,0 +1,7 @@
+Project is set up using Parcel Bundler.
+
+1. Install all the required packages mentioned in package.json
+npm install
+
+2. To run the application 
+npm run start
